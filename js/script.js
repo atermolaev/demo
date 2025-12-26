99d3e1bceb1c34d8893e241f8f1f971197d7370b
Anton Termolaev <a.termolaev@mail.ru>
2025-12-26 14:07:50 +0300

    Add constuctor link - super

--- js/script.js
+++ js/script.js
@@ -1,21 +1,29 @@
-Object.prototype.inherit = function(Parent){
-    const F = function() { }
-    F.prototype = Parent.prototype
-    this.prototype = new F()
-    this.prototype.constructor = this
-    this.superclass = Parent.prototype
-}
+Object.prototype.inherit = function (Parent) {
+  const F = function () {};
+  F.prototype = Parent.prototype;
+  this.prototype = new F();
+  this.prototype.constructor = this;
+  this.superclass = Parent.prototype;
+  // Добавляем ссылку на родительский конструктор
+  this.prototype._super = function () {
+    Parent.apply(this, arguments);
+  };
+};
 
 function Main() {
-
+  this.a = 100;
 }
 
-Main.prototype.method = function(param){
-    return `param: ${param}`;
-}
+Main.prototype.method = function (param) {
+  return `param: ${param}`;
+};
 
 function Box() {
-
+  this._super();
 }
 
 Box.inherit(Main);
+
+Box.prototype.getMainProps = function () {
+  return this;
+};
